1546a07f8b20f5c8b6eb4d0a7764cd07e17195bb
jperales <jperales.bioinfo@gmail.com>
2020-06-18 09:44:00 +0200

    script to get R dep

---
+++ R/Rdep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# src: https://github.com/mschubert/ebits/blob/master/dependencies.sh
+scripts=$(find . -name "*.r" -o -name "*.R")
+content=$(echo $scripts | xargs cat)
+
+regex=(
+    "(?<=library\()[a-zA-Z0-9.]+|"
+    "(?<=require\()[a-zA-Z0-9.]+|"
+    "(?<=requireNamespace\()[a-zA-Z0-9.]+|"
+    "(?<=import_package\()[a-zA-Z0-9.]+|"
+    "[a-zA-Z0-9.]+(?=:::?)"
+)
+combined=$(printf "%s" "${regex[@]}")
+
+echo $content | tr -d "'\"" | grep -o -P "$combined" | sort | uniq
